85c1ddd366a3c0464b28725777337c5695a97c54
hivini <vinicioquinterobuss@gmail.com>
2020-05-12 08:59:11 -0600

    Added condition that there has to be a valid email

--- users/user.service.js
+++ users/user.service.js
@@ -7,6 +7,9 @@ module.exports = {
 };
 
 async function registerUser(userParam) {
+    if (userParam.email == null || userParam.email === '') {
+        throw Error("Non a valid email");
+    }
     // Replace the original password with a hash
     if (userParam.password) {
         userParam.password = bcrypt.hashSync(userParam.password, 10);
